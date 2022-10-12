4bbbab2b9637ea25fb01cb68ac577bd2315d5e40
taldoflemis <coderflemis@gmail.com>
2022-10-12 13:43:43 -0300

    adding custom fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        logo: ["aassassinninjaregular"],
       },
       colors: {
         primary: "var(--color-primary)",
