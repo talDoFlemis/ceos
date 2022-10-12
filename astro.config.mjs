b00c4828e996fa93fda2556026d69a68d5e4b042
taldoflemis <coderflemis@gmail.com>
2022-10-12 09:29:38 -0300

    adding prettier and eslint with some types

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
   vite: {
     ssr: {
       external: ["@11ty/eleventy-img", "svgo"],
